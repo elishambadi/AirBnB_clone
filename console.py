bff6f9fe7b91ea37bdcc78bc7cb1bbc33e70acc3
Elisha Mbadi <embadi43@gmail.com>
2022-08-31 12:20:40 +0300

    add console v 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+'''
+   Console program for Airbnb
+'''
 
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    
+    def do_foo(self, name):
+        '''Prints foo
+	'''
+        print("Foo; {}".format(name))
+
+    def do_quit(self, id):
+        '''Quit command to exit the program
+	'''
+        exit(0)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
